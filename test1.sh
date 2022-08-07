2d63866883913fce6d0d128d867e3d4abfe933ae
ebi2050 <89188880+ebioki2050@users.noreply.github.com>
2022-08-07 19:10:41 +0900

    2022-08-07_change_style_of_sh_file

--- test1.sh
+++ test1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname ${0})
 rm send_in.txt 
-/home/ebi2050/developments/mailing_md/send_mail.sh
+/home/ebi2050/developments/mailing_md/execute1.sh
--- test2.sh
+++ test2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $(dirname ${0})
 rm send_in_lines.txt
-/home/ebi2050/developments/mailing_md/send_mail_lines.sh
+/home/ebi2050/developments/mailing_md/execute2.sh
